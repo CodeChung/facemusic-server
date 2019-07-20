8096dbd782cccbf4fef59cc94ec859a0cfcd6f2f
Harry Chung <hc9825@gmail.com>
2019-07-19 23:29:27 -0700

    implemented deletePhoto

--- src/photos/photo-service.js
+++ src/photos/photo-service.js
@@ -3,34 +3,6 @@ const request = require('request')
 const faceApiEndpoint = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect?'
 
 
-
-//should i keep this in the object
-//sends photo url to face api for analysis
-// analyzePhoto = (url) => {
-//     const params = {
-//         'returnFaceId': 'true',
-//         'returnFaceLandmarks': 'false',
-//         'returnFaceAttributes': 'emotion'
-//     }
-//     const options = {
-//         uri: faceApiEndpoint,
-//         qs: params,
-//         body: '{"url": ' + '"' + url + '"}',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Ocp-Apim-Subscription-Key': process.env.FACE_API_KEY
-//         }
-//     }
-//     request.post(options, (error, response, body) => {
-//         if (error) {
-//           console.log('Error: ', error);
-//           return
-//         }
-//         let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ')
-//         return jsonResponse
-//     })
-// }
-
 //sends photo url to Face API, returns emotion data json
 analyzePhoto = (photoData) => {
     return new Promise(function(resolve, reject) {
@@ -53,15 +25,13 @@ analyzePhoto = (photoData) => {
               console.log('Error: ', error);
               reject(error)
             }
-            let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ')
-            console.log('length', body.length)
-            console.log(body)
-            console.log(typeof body)
-            console.log(body[0])
-            if (body.length === 0) {
-                resolve(['yolo'])
+            let jsonResponse = JSON.parse(body)
+            //empty array == no facial data, so let's delete photo to save space
+            if (jsonResponse.length === 0) {
+                deletePhoto(photoData.public_id)
+                resolve({error: {message: 'Face is not recognized, please try again'}})
             }
-            resolve(jsonResponse)
+            resolve(jsonResponse[0])
         })
     })
 }
@@ -74,17 +44,16 @@ photoToUrl = (uri) => {
                 if (error) {
                     throw new Error(error)
                 }
-                const {url, delete_token} = result
-                //make call to Face API and store in photoinfo
-                resolve({url, delete_token})
+                const {url, public_id} = result
+                resolve({url, public_id})
             }
         )
     })
 }
 
-deletePhoto = (delete_token) => {
-    console.log('delete')
-    console.log(delete_token)
+deletePhoto = (public_id) => {
+    cloudinary.v2.uploader.destroy(public_id, function(error,result) {
+        console.log(result, error) });
 }
 
 const photoService = {
